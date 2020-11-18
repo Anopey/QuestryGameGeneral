a576b22112f92fe81800f2ca32f7428e3c0a804e
Ozan Ürkmez <arkantos250@gmail.com>
2020-11-18 19:20:59 +0300

    implement a reflection utility

---
+++ ReflectionUtilities.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace QuestryGameGeneral
+{
+    public static class ReflectionUtilities
+    {
+
+
+        public static Type GetMostPrimitive(Type t)
+        {
+            Type returned = t;
+            while (true)
+            {
+                Type temp = t.BaseType;
+                if (temp == null)
+                    break;
+                returned = temp;
+            }
+            return returned;
+        }
+
+    }
+}
