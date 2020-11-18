ac725390d2f04f6196a85bbfc18f7976a1d08264
Ozan Ürkmez <arkantos250@gmail.com>
2020-11-18 19:25:55 +0300

    fix bug and fix utility

--- ReflectionUtilities.cs
+++ ReflectionUtilities.cs
@@ -9,10 +9,17 @@ namespace QuestryGameGeneral
     public static class ReflectionUtilities
     {
 
-
+        /// <summary>
+        /// Returns the most base type in the inheritance tree that this Type inherits from.
+        /// </summary>
+        /// <param name="t"></param>
+        /// <returns></returns>
         public static Type GetMostPrimitive(Type t)
         {
             Type returned = t;
+            if (returned == null)
+                return returned;
+
             while (true)
             {
                 Type temp = t.BaseType;
